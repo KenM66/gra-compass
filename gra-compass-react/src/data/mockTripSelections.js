f93a84a5c171b3a739f9d47832e5e291bdb89146
Kenneth Milota <kenm661@gmail.com>
2026-09-23 18:23:43 -0400

    updated mock data files.

--- gra-compass-react/src/data/mockTripSelections.js
+++ gra-compass-react/src/data/mockTripSelections.js
@@ -112,6 +112,106 @@ const selectionsByTrip = {
       ],
     },
   ],
+  2: [
+    {
+      id: 6,
+      type: "Excursion",
+      name: "Glacier Wildlife Cruise",
+      sessions: [
+        {
+          id: 601,
+          date: "2027-06-15",
+          time: "09:00",
+          maxCapacity: 30,
+          active: true,
+        },
+        {
+          id: 602,
+          date: "2027-06-15",
+          time: "13:00",
+          maxCapacity: 30,
+          active: true,
+        },
+      ],
+    },
+    {
+      id: 7,
+      type: "Excursion",
+      name: "Alaskan Scenic Railway",
+      sessions: [
+        {
+          id: 701,
+          date: "2027-06-16",
+          time: "08:30",
+          maxCapacity: 40,
+          active: true,
+        },
+        {
+          id: 702,
+          date: "2027-06-16",
+          time: "13:30",
+          maxCapacity: 40,
+          active: true,
+        },
+      ],
+    },
+    {
+      id: 8,
+      type: "Excursion",
+      name: "Denali Wilderness Tour",
+      sessions: [
+        {
+          id: 801,
+          date: "2027-06-17",
+          time: "08:00",
+          maxCapacity: 24,
+          active: true,
+        },
+        {
+          id: 802,
+          date: "2027-06-17",
+          time: "13:00",
+          maxCapacity: 24,
+          active: true,
+        },
+      ],
+    },
+    {
+      id: 9,
+      type: "Spa",
+      name: "Spa Treatment",
+      sessions: [
+        {
+          id: 901,
+          date: "2027-06-16",
+          time: "11:00",
+          maxCapacity: 8,
+          active: true,
+        },
+        {
+          id: 902,
+          date: "2027-06-16",
+          time: "14:00",
+          maxCapacity: 8,
+          active: true,
+        },
+      ],
+    },
+    {
+      id: 10,
+      type: "Event",
+      name: "Alaska Welcome Dinner",
+      sessions: [
+        {
+          id: 1001,
+          date: "2027-06-14",
+          time: "18:30",
+          maxCapacity: 100,
+          active: true,
+        },
+      ],
+    },
+  ],
 };
 
 export default selectionsByTrip;
